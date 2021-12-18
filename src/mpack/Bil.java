c35cecf3f1b8358af728a3edbff011e025a8053d
AMINE ABOU-ELOUAFA <abouelouafa.95@gmail.com>
2021-12-18 16:28:28 +0100

    update2

--- src/mpack/Bil.java
+++ src/mpack/Bil.java
@@ -1,4 +1,25 @@
 package mpack;
 
+import java.util.Map;
+
 public class Bil {
+   private Client customer;
+    private Map<Product,Integer> products;
+
+    public void addProduct(Product produit,Integer quantity){
+        this.products.put(produit,quantity);
+
+    }
+
+    public Client getCustomer() {
+        return customer;
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
+
+    public Bil(Client customer) {
+        this.customer = customer;
+    }
 }
